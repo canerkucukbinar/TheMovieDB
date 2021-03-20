72f95bef09b6c5aae4eb1f893e11241dcf87abaf
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-20 19:05:53 +0300

    tvshow response refactored for detail page

--- app/src/main/java/com/ckucukbinar/themoviewdb/data/model/TVShowResponse.kt
+++ app/src/main/java/com/ckucukbinar/themoviewdb/data/model/TVShowResponse.kt
@@ -14,5 +14,11 @@ data class TVShowResponse(
     @SerializedName("id")  val id: Int?,
     @SerializedName("name") val name: String?,
     @SerializedName("vote_average") val voteAverage: Double?,
-    @SerializedName("poster_path") val imagePath: String?
+    @SerializedName("poster_path") val imagePath: String?,
+    @SerializedName("overview") val overview: String?,
+    @SerializedName("first_air_date") val firstAirDate: String?,
+    @SerializedName("origin_country") val originCountry: String?,
+    @SerializedName("original_language") val originalLanguage: String?,
+    @SerializedName("original_name") val originalName: String?,
+    @SerializedName("vote_count") val voteCount: Int?,
 )
