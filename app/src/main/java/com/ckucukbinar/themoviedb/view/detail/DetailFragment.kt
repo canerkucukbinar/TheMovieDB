942b06496e024b45e5d8d3af75f0b0423bca5dea
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-21 22:10:24 +0300

    detail fragment moved

---
+++ app/src/main/java/com/ckucukbinar/themoviedb/view/detail/DetailFragment.kt
@@ -0,0 +1,11 @@
+package com.ckucukbinar.themoviedb.view.detail
+
+import androidx.fragment.app.Fragment
+import com.ckucukbinar.themoviedb.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+}
--- app/src/main/java/com/ckucukbinar/themoviewdb/view/detail/DetailFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.ckucukbinar.themoviewdb.view.detail
-
-import androidx.fragment.app.Fragment
-import com.ckucukbinar.themoviedb.R
-
-class DetailFragment : Fragment(R.layout.fragment_list) {
-
-}
