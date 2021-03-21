91ae7369618266691f9a71b596506792986b489b
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-21 14:12:42 +0300

    list and detail fragment added

---
+++ app/src/main/java/com/ckucukbinar/themoviewdb/view/detail/DetailFragment.kt
@@ -0,0 +1,8 @@
+package com.ckucukbinar.themoviewdb.view.detail
+
+import androidx.fragment.app.Fragment
+import com.ckucukbinar.themoviedb.R
+
+class DetailFragment : Fragment(R.layout.fragment_list) {
+
+}
---
+++ app/src/main/java/com/ckucukbinar/themoviewdb/view/list/ListFragment.kt
@@ -0,0 +1,8 @@
+package com.ckucukbinar.themoviewdb.view.list
+
+import androidx.fragment.app.Fragment
+import com.ckucukbinar.themoviedb.R
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+}
