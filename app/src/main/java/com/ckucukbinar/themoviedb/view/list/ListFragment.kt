7ff2f57f107c803a09185f7dbaa88d3093735c78
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-21 22:11:59 +0300

    list fragment moved and view binding added

---
+++ app/src/main/java/com/ckucukbinar/themoviedb/view/list/ListFragment.kt
@@ -0,0 +1,42 @@
+package com.ckucukbinar.themoviedb.view.list
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.ckucukbinar.themoviedb.R
+import com.ckucukbinar.themoviedb.databinding.FragmentListBinding
+import com.ckucukbinar.themoviedb.view.list.adapter.PopularTVShowListAdapter
+import com.ckucukbinar.themoviedb.view.list.adapter.viewmodel.ListViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListFragment : Fragment(R.layout.fragment_list) {
+    private val viewModel by viewModels<ListViewModel>()
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+    private var adapter: PopularTVShowListAdapter? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentListBinding.bind(view)
+        initView()
+        viewModel.popularTVShowList.observe(viewLifecycleOwner) { pagingData ->
+            adapter?.run {
+                this.submitData(viewLifecycleOwner.lifecycle, pagingData)
+            }
+        }
+    }
+
+    private fun initView() {
+        binding.apply {
+            rvList.apply {
+                setHasFixedSize(true)
+                this@ListFragment.adapter?.run {
+                    adapter = this
+                }
+            }
+        }
+    }
+
+}
