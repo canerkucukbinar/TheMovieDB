9041300450fc4dc18a729211c5153858e8d617c1
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-22 22:19:59 +0300

    TMDBservice interface refactored

--- app/src/main/java/com/ckucukbinar/themoviedb/data/service/TMDBService.kt
+++ app/src/main/java/com/ckucukbinar/themoviedb/data/service/TMDBService.kt
@@ -1,9 +1,6 @@
 package com.ckucukbinar.themoviedb.data.service
 
-import com.ckucukbinar.themoviedb.model.PopularTVShowResult
-import com.ckucukbinar.themoviedb.model.TVShow
-import com.google.gson.annotations.SerializedName
-import retrofit2.Response
+import com.ckucukbinar.themoviedb.data.model.PopularTVShowResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -17,20 +14,22 @@ interface TMDBService {
 
 
         object TV {
-            const val maintPath = "$baseURL/tv"
-            const val popular = "$maintPath/popular"
-            const val detail = "$maintPath/{tv_id}"
+            private const val maintPath = "tv/"
+            const val popular = "${maintPath}popular"
+            const val detail = "${maintPath}{tv_id}"
         }
     }
 
-    @GET("${Endpoint.TV.detail}?api_key=${Endpoint.apiKey}")
+    @GET(Endpoint.TV.detail)
     fun getTVShowDetail(
+        @Query("api_key") apiKey: String?,
         @Query("language") language: String?
     ) {
     }
 
-    @GET("${Endpoint.TV.popular}?api_key=${Endpoint.apiKey}")
+    @GET(Endpoint.TV.popular)
     suspend fun getPopularTVShowList(
+        @Query("api_key") apiKey: String?,
         @Query("page") page: Int?,
         @Query("language") language: String? = null
     ) : PopularTVShowResult
