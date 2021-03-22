8a44a70e71178b360dc685af89f71bffaf489c02
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-23 00:31:21 +0300

    list view model cachedIn call added

--- app/src/main/java/com/ckucukbinar/themoviedb/view/list/adapter/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/ckucukbinar/themoviedb/view/list/adapter/viewmodel/ListViewModel.kt
@@ -2,8 +2,10 @@ package com.ckucukbinar.themoviedb.view.list.adapter.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.ckucukbinar.themoviedb.data.repository.TMDBRepository
 
 class ListViewModel @ViewModelInject constructor(private val tmdbRepository: TMDBRepository) : ViewModel() {
-    val popularTVShowList = tmdbRepository.getPopularTvShowList()
+    val popularTVShowList = tmdbRepository.getPopularTvShowList().cachedIn(viewModelScope)
 }
