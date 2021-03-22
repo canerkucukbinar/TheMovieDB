50cc6924a968a8bddfb0d3f9b7bd0d0a9e2bbc42
Caner Küçükbinar <canerkucukbinar@gmail.com>
2021-03-23 00:29:06 +0300

    unused service removed

--- app/src/main/java/com/ckucukbinar/themoviedb/data/service/TMDBService.kt
+++ app/src/main/java/com/ckucukbinar/themoviedb/data/service/TMDBService.kt
@@ -16,15 +16,7 @@ interface TMDBService {
         object TV {
             private const val maintPath = "tv/"
             const val popular = "${maintPath}popular"
-            const val detail = "${maintPath}{tv_id}"
         }
-    }
-
-    @GET(Endpoint.TV.detail)
-    fun getTVShowDetail(
-        @Query("api_key") apiKey: String?,
-        @Query("language") language: String?
-    ) {
     }
 
     @GET(Endpoint.TV.popular)
